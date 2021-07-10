cffa5222bfcb1d77741bb8e7be8ade4fb3fd26ce
Diana Zaharova <Dysua1999@mail.ru>
2021-07-10 18:10:09 +0300

    feat: add fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "typeface-open-sans";
+import "./fonts/fonts.css";
 import App from "./components/App/App.jsx";
 
 ReactDOM.render(
